7255f743899d3b3031c33b2b51f15f50dce30b9a
Saravanan Anbalagan <saravananan@maveric-systems.com>
2021-12-07 17:20:01 +0530

    second commit

---
+++ src/demo1/Addpgm.java
@@ -0,0 +1,14 @@
+package demo1;
+
+public class Addpgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=5;
+int b=5;
+int c;
+c= a+b;
+System.out.println("the add number is " +c);
+	}
+
+}
