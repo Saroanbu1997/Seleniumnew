0834e0095e4d514da77c3ab2e34370691f86610d
Saravanan Anbalagan <saravananan@maveric-systems.com>
2021-12-09 15:40:37 +0530

    third commit

---
+++ src/demo1/Subpgm.java
@@ -0,0 +1,16 @@
+package demo1;
+
+public class Subpgm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+int a=6;
+int b=5;
+int c;
+c= a-b;
+System.out.println("the add number is " +c);
+	}
+	}
+
+
